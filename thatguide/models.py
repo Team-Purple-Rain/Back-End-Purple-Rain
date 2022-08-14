2120a51c6beac47c88585974f051b935490d5f22
ryanfrenia <ryanfrenia@gmail.com>
2022-08-14 17:45:16 -0400

    changed model field values, created validator for user location in serializer

--- thatguide/models.py
+++ thatguide/models.py
@@ -1,13 +1,16 @@
-
 from django.db import models
 
 
 class TimeStamp(models.Model):
     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
 
 
 class HikingSession(TimeStamp):
+    #id = models.BigAutoField(primary_key=True)
     #hike_user = models.ForeignKey('User', related_name='hiker', on_delete=models.CASCADE)
     one = 1
     five = 5
@@ -19,10 +22,11 @@ class HikingSession(TimeStamp):
     ]
     distance_list = models.IntegerField(choices=distance_choice, default=one)
     #location is JSONField so that front-end can pass the object
-    start_location = models.JSONField
-    end_location = models.JSONField
-    distance_traveled = models.IntegerField
-    avg_mph = models.IntegerField
+    start_location = models.JSONField(null=False, blank=False)
+    end_location = models.JSONField(null=True, blank=True)
+    distance_traveled = models.IntegerField(null=True, blank=True)
+    avg_mph = models.IntegerField(null=True, blank=True)
     #travel_time is an integerfield in secconds. Total amount to be divded by 60 to get minutes. 
-    travel_time = models.IntegerField
-    elevation_gain = models.IntegerField
+    travel_time = models.IntegerField(null=True, blank=True)
+    elevation_gain = models.IntegerField(null=True, blank=True)
+    
