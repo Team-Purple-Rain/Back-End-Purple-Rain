b40e56506db05711d1fe0d4e326adc5d0654c812
Caelll <cael.blackmon1@gmail.com>
2022-08-22 10:00:04 -0400

    added models to edit in the admin page

--- thatguide/admin.py
+++ thatguide/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import HikingSession, HikingCheckPoint
 # Register your models here.
+
+admin.site.register(HikingSession)
+admin.site.register(HikingCheckPoint)
